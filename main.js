e4b46f62c20ff6098c97cf053d9cdca0a64e85cb
Pigpig105 <81789882+Pigpig105@users.noreply.github.com>
2023-05-20 02:06:45 -0400

    Update main.js
    Fixed popup issue.
    
    PS: Works!  You can send coins through Q-App!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Open the popup when the button is clicked
 document.getElementById("myButton").addEventListener("click", function() {
     document.getElementById("myPopup").style.display = "block";
-  });
+});
   
   // Close the popup when the user clicks outside of it
   window.addEventListener("click", function(event) {
@@ -9,19 +9,22 @@ document.getElementById("myButton").addEventListener("click", function() {
     if (event.target == popup) {
       popup.style.display = "none";
     }
-  });
+});
 
-let SEND_COIN_destinationAddress;
-let SEND_COIN_amount;
 document.getElementById("SEND_QORT_Submit").onclick = function(){
     SEND_COIN_destinationAddress = document.getElementById("SEND_COIN_destinationAddress").value;
     SEND_COIN_amount = document.getElementById("SEND_COIN_amount").value;
     SEND_COIN_amount = Number(SEND_COIN_amount);
+    sendCoin();
 }
 
-let res = await qortalRequest({
-    action: "SEND_COIN",
-    coin: "QORT",
-    destinationAddress: SEND_COIN_destinationAddress,
-    amount: SEND_COIN_amount
-});
+async function sendCoin() {
+    let res = await qortalRequest({
+      action: "SEND_COIN",
+      coin: "QORT",
+      destinationAddress: SEND_COIN_destinationAddress,
+      amount: SEND_COIN_amount
+    });
+  }
+  
+  setTimeout(sendCoin, 0);
